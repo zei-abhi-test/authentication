2907fbf41f17a98b4a6d621d8c974389a8137448
zei-abhi-test <usetoshiba27@gmail.com>
2026-03-06 16:50:00 +0530

    EOD

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,32 @@
+import { Link } from "react-router-dom"
+import ConnectionTest from "../components/ConnectionTest"
+
 function Home() {
   return (
     <div>
-      <h2>Home Page</h2>
-      <p>Welcome to the Creator Platform where users can share their content.</p>
-      <ConnectionTest />
+      <h1>Creator Platform</h1>
+      <p>Welcome to the Creator Platform where users can share and manage their content.</p>
+
+      <h3>Navigate</h3>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Go to Login</Link>
+          </li>
+
+          <li>
+            <Link to="/register">Go to Register</Link>
+          </li>
+
+          <li>
+            <Link to="/dashboard">Go to Dashboard</Link>
+          </li>
+          <li>
+            <ConnectionTest />
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
