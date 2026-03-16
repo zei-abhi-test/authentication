735c0d0e0f6a253a40db6f2cf517ee364e49b73c
zei-abhi-test <usetoshiba27@gmail.com>
2026-03-16 16:30:03 +0530

    authcontext and authprovider

---
+++ frontend/src/context/authContext.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContext } from "react";
+
+// Just the context and the hook here
+export const AuthContext = createContext(null);
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
