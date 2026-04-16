e3a34442461297153d6b9fb2e639212e9af70439
zei-abhi-test <usetoshiba27@gmail.com>
2026-04-16 17:07:22 +0530

    Add index to email field for faster login queries

--- Backend/src/models/User.js
+++ Backend/src/models/User.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, },
+    name: { type: String, required: true },
+
     email: {
       type: String,
       required: true,
       unique: true,
+      index: true
     },
-    password: { type: String, required: true,  }
+
+    password: { type: String, required: true }
   },
   { timestamps: true }
 )
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema)
